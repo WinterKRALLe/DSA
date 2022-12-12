beb0c456f83d2e05ddbeed08b89584408b037299
WinterKRALLe <82891169+WinterKRALLe@users.noreply.github.com>
2022-12-12 21:34:08 +0100

    final

--- dsa.py
+++ dsa.py
@@ -1,78 +1,47 @@
-import shutil
-from rsa import generateKeys
+import rsa
 import hashlib, sys, os.path, time, pathlib
 import assets
-
 from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PyQt6 import uic
 
-X = 12
-z = 10
-block = z * X
-
-BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
 
 def podpis(soubor, klic):
-
-    sha3 = hashlib.sha3_512()
-    with open(soubor, 'rb') as f:
-        while True:
-            data = f.read(BUF_SIZE)
-            if not data:
-                break
-            sha3.update(data)
-    hash = sha3.hexdigest()
+    with open(soubor, "rb") as f:
+        novyHash = hashlib.sha3_512(f.read()).hexdigest()
 
     with open(klic, "r") as f:
-        klic = f.read()
+        klicek = f.read()
 
-    n = int(klic[0])
-    d = int(klic[1])
+    n, d = rsa.decodeKeyBase64(klicek)
 
-    OTblocks = [ord(char) for char in hash]
-    BINblocks = [bin(ch)[2:].zfill(X) for ch in OTblocks]
-    BIN = "".join(BINblocks)
-    BINs = [BIN[i:i + block] for i in range(0, len(BIN), block)]
-    INTblocks = [int(ch, 2) for ch in BINs]
-    c = [pow(ch, d, n) for ch in INTblocks]
-    signature = "".join([str(ch) for ch in c])
-    return signature
+    signature = rsa.encode(novyHash, n, d)
+    s = rsa.encodeBase64(signature)
+    return s
 
 
 def overeni(soubor, klic, sign):
-    sha3 = hashlib.sha3_512()
     with open(soubor, "rb") as f:
-        while True:
-            data = f.read(BUF_SIZE)
-            if not data:
-                break
-            sha3.update(data)
-    ohash = sha3.hexdigest()
+        novyHash = hashlib.sha3_512(f.read()).hexdigest()
 
     with open(klic, "r") as f:
-        klic = f.read()
+        klicek = f.read()
 
-    n = int(klic[0])
-    e = int(klic[1])
+    n, e = rsa.decodeKeyBase64(klicek)
 
     with open(sign, "r") as f:
         signature = f.read()
 
-    INTblocks = [int(ch) for ch in signature]
-    m = [pow(c, e, n) for c in INTblocks]
-    BINblocks = [bin(ch)[2:].zfill(block) for ch in m]
-    BIN = "".join(BINblocks)
-    BINs = [BIN[i:i + X] for i in range(0, len(BIN), X)]
-    INTs = [int(ch, 2) for ch in BINs]
-    output = [chr(ch) for ch in INTs]
-    output = "".join(output)
-    
-    if ohash == output:
+    decSignature = rsa.decodeBase64(signature)
+    druhy = rsa.decode(decSignature, n, e)
+
+    print("hash1: ", novyHash)
+    print("hash2: ", druhy)
+
+    if druhy == novyHash:
         msg = "Jsou shodné"
-        return msg, soubor
     else:
         msg = "Nejsou shodné"
-        return msg, soubor
+    return msg, soubor
 
 
 def properties(fileName):
@@ -129,9 +98,8 @@ class MyApp(QMainWindow, Ui_MainWindow):
 
     def podpisSave(self):
         signature = podpis(podpisSoubor, privateKey)
-        # archived = shutil.make_archive() zip file
 
-        soubor, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getOpenFileName()", "","Privatni klic (*.sign);;All Files (*)")
+        soubor, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getOpenFileName()", "RSA_SHA3-512 PODPIS_V_BASE64.sign","Signature (*.sign);;All Files (*)")
         if soubor:
             with open(soubor, "w") as f:
                 f.write(signature)
@@ -140,7 +108,7 @@ class MyApp(QMainWindow, Ui_MainWindow):
     def otevriPublicKey(self):
         global publicKey
         try:
-            publicKey, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","Public key (*.pub);;All Files (*)")
+            publicKey, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","(*.pub);;All Files (*)")
             msg = "" + os.path.basename(publicKey) + " byl nahrán"
             self.message.setText(msg)
 
@@ -167,7 +135,7 @@ class MyApp(QMainWindow, Ui_MainWindow):
     def overeniOtevriPodpis(self):
         global overeniPodpis
         try:
-            overeniPodpis, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*)")
+            overeniPodpis, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","(*.sign);;All Files (*)")
             msg = "" + os.path.basename(overeniPodpis) + " byl nahrán"
             self.message.setText(msg)
         except:
@@ -176,19 +144,21 @@ class MyApp(QMainWindow, Ui_MainWindow):
         msg, _ = overeni(overeniSouboru, publicKey, overeniPodpis)
         self.message.setText(msg)
 
+
     def keys(self):
-        n, e, d = generateKeys()
-        privateKey = (str(n) + ", " + str(d))
-        publicKey = (str(n) + ", " + str(e))
-        fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getOpenFileName()", "","Privatni klic (*.priv);;All Files (*)")
+        n, e, d = rsa.generateKeys()
+        privateKey = rsa.encodeKeyBase64(n, d)
+        publicKey = rsa.encodeKeyBase64(n, e)
+        fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getOpenFileName()", "RSA SOUKROMÝ_KLÍČ_V_BASE64.priv","(*.priv);;All Files (*)")
         if fileName:
             with open(fileName, "w") as f:
                 f.write(privateKey)
-        fileName1, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getOpenFileName()", "","Verejny klic (*.pub);;All Files (*)")
+        fileName1, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getOpenFileName()", "RSA VEŘEJNÝ_KLÍČ_V_BASE64.pub","(*.pub);;All Files (*)")
         if fileName1:
             with open(fileName1, "w") as f:
                 f.write(publicKey)
             
+
     def __init__(self):
         QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
--- rsa.py
+++ rsa.py
@@ -1,4 +1,9 @@
-import random, math
+import random, math, base64
+
+X = 12
+z = 10
+block = z * X
+
 
 def miller_rabin(n):
     if n % 2 == 0:
@@ -33,3 +38,56 @@ def generateKeys():
                 break
         d = pow(e, -1, Fn)
         return(n,e,d)
+
+
+def encode(text, n, d):
+    OTblocks = [ord(char) for char in text]
+    BINblocks = [bin(ch)[2:] for ch in OTblocks]
+    BIN = "".join(BINblocks)
+    filled = len(BIN) % block
+    if filled:
+        BIN = "0"*(block - filled) + BIN
+    BINs = [BIN[i:i + block] for i in range(0, len(BIN), block)]
+    INTblocks = [int(ch, 2) for ch in BINs]
+    c = [pow(ch, d, n) for ch in INTblocks]
+    signature = " ".join([str(ch) for ch in c])
+    return signature
+
+
+def decode(text, n, e):
+    INTblocks = [int(ch) for ch in text.split(" ")]
+    m = [pow(c, e, n) for c in INTblocks]
+    BINblocks = [bin(ch)[2:].zfill(block) for ch in m]
+    BIN = "".join(BINblocks)
+    BINs = [BIN[i:i + X] for i in range(0, len(BIN), X)]
+    oBINs = [char for char in BINs if int(char) != 0]
+    INTs = [int(ch, 2) for ch in oBINs]
+    output = [chr(ch) for ch in INTs]
+    output = "".join(output)
+    return output
+
+
+def encodeBase64(text):
+    sample_string_bytes = text.encode("ascii")
+    base64_bytes = base64.b64encode(sample_string_bytes)
+    base64_string = base64_bytes.decode("ascii")
+    return base64_string
+
+
+def decodeBase64(text):
+    base64_bytes = text.encode("ascii")
+    sample_string_bytes = base64.b64decode(base64_bytes)
+    sample_string = sample_string_bytes.decode("ascii")
+    return sample_string
+
+
+def encodeKeyBase64(fi, se):
+	key = str(fi) + " " + str(se)
+	encoded = encodeBase64(key)
+	return encoded
+
+
+def decodeKeyBase64(key):
+	decoded = decodeBase64(key)
+	fi, se = decoded.split()
+	return int(fi), int(se)
